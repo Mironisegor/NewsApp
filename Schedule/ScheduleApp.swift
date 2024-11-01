8b852d2a40fec1fc0982a924a3a6f86cde4aa1f3
Egor <mironove55@icloud.com>
2024-11-01 13:31:52 +0300

    Initial Commit

---
+++ Schedule/ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleApp.swift
+//  Schedule
+//
+//  Created by GRXXVY production on 01.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
