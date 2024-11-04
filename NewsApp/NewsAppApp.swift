ead54be530ca9b6cd305b6cb91e96af24ac3aa28
Mironisegor <mironove55@icloud.com>
2024-11-04 11:44:36 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Vladimir Fursov on 04.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
